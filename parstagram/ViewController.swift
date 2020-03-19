71759a091f159b6bd64c4a76b9e681784fdfe82d
Spencer Edwards <Spencer.Edwards@SJSU.EDU>
2020-03-19 11:02:10 -0700

    Initial Commit

---
+++ parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parstagram
+//
+//  Created by Spencer on 3/19/20.
+//  Copyright © 2020 Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
